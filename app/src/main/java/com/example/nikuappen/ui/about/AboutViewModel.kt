35a8a52f11c8ea3e012558a8db355756e0dc32eb
Martin Sederholm <sederholm.martin@gmail.com>
2019-12-18 09:23:00 +0200

    added settings menu items

--- app/src/main/java/com/example/nikuappen/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/example/nikuappen/ui/about/AboutViewModel.kt
@@ -3,11 +3,14 @@ package com.example.nikuappen.ui.about
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.nikuappen.R
+import kotlinx.android.synthetic.main.fragment_about.view.*
 
 class AboutViewModel : ViewModel() {
 
+
     private val _text = MutableLiveData<String>().apply {
-        value = "This is about Fragment"
+        //value = context.getResources().getString(R.string.about_text)
     }
     val text: LiveData<String> = _text
 }
